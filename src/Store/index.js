4e29de7add18eb7abeb28238eff2537293af5536
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-03-23 19:59:41 -0700

    implemented feature section

--- src/Store/index.js
+++ src/Store/index.js
@@ -12,8 +12,13 @@ const useMacbookStore = create((set)=>({
     // Function to change scale
     setScale: (scale) => set({ scale }),
 
+
+// Modifying the texture of the Macbook in the Features component
+    texture: '/videos/feature-1.mp4',
+    setTexture: (texture) => set({ texture }),
+
     // Resets everything back to default
-    reset:()=> set({ color: '#2e2c2e', scale:0.08 }),
+    reset:()=> set({ color: '#2e2c2e', scale:0.08, texture: '/videos/feature-1.mp4' }),
 }))
 
 export default useMacbookStore;
